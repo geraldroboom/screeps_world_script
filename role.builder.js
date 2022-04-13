1dfb604e94d1f349d57be9df845f8afdfd5bcbb6
geraldroboom <60895817+geraldroboom@users.noreply.github.com>
2022-04-13 17:33:31 +0200

    init, tutorial branch 5

---
+++ role.builder.js
@@ -0,0 +1,30 @@
+var roleBuilder = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+	    }
+	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.building = true;
+	    }
+
+	    if(creep.memory.building) {
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+	    }
+	    else {
+	        var s = Game.spawns['Spawn1'];
+            if(creep.withdraw(s, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(s);
+            }
+	    }
+	}
+};
+
+module.exports = roleBuilder;
