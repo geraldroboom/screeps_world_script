a9ed53740a1c96ec58b8e51b54ffe41f113e63ed
geraldroboom <60895817+geraldroboom@users.noreply.github.com>
2022-09-05 15:33:23 +0200

    finished spawning logic

--- main.js
+++ main.js
@@ -7,10 +7,39 @@ var roleMilitia = require('role.militia');
 
 var analyserResources = require('analyser.resources');
 
+var managerCreepSpawner = require('manager.creep_spawner');
+
 module.exports.loop = function () {
 
-    if(Game.time == 1) {
-        analyserResources.run();
+    /* Initializing Variables to communicate with the programm. */
+    if (Memory.cmd.analyser === undefined)
+        Memory.cmd.analyser = {};
+
+    if (Memory.cmd.spawner === undefined)
+        Memory.cmd.spawner = {};
+
+    if (Memory.cmd.analyser['all'] === undefined) 
+        Memory.cmd.analyser['all'] = true;
+
+    if (Memory.cmd.spawner['active'] === undefined) 
+        Memory.cmd.spawner['active'] = true;
+
+    /* Checking if there is a command to analyze something. */
+    if(Memory.cmd.analyser['all']) {
+        for(let roomName in Memory.rooms) {
+            analyserResources.run(Game.rooms[roomName]);
+        }
+
+        Memory.cmd.analyser['all'] = false;
+    }
+
+    /* Checking if there is an idle spawn and then wether or not another creep needs to be spawned */
+    if(Memory.cmd.spawner['active']) {
+        for(var i in Game.spawns) {
+            if(! (Game.spawns[i].spawning)) {
+                managerCreepSpawner.run(i);
+            }
+        }
     }
 
     /* Cycling throug all creep-roles and performs their programms. */
--- manager.creep_spawner.js
+++ manager.creep_spawner.js
@@ -1,46 +1,228 @@
 
 
 var managerCreepSpawner = {
+    spawnName: undefined,
+    // "maxStationaryHarvestersPerSource"
+    maxSHpS: 4,
 
-    run: function() {
-        this.spawn('test');
+    run: function(spawn) {
+        this.spawnName = spawn;
+        if (this.missing_stationary_harvester()) {}
+        else if(this.missing_carriers()) {}
+        else if(this.missing_upgrader()) {}
+        else if(this.missing_builder()) {}
+        else if(this.missing_militia()) {}
+        
+    },
+
+    getName: function(role) {
+        if (Memory.nameIndex === undefined)
+            Memory.nameIndex = {};
+
+        if (Memory.nameIndex[role] === undefined) 
+            Memory.nameIndex[role] = 0;
+
+        return role + (Memory.nameIndex[role] + 1);
+    },
+
+    commitName: function(role) {
+        var newIndex = Memory.nameIndex[role] + 1;
+        Memory.nameIndex[role] = newIndex;
     },
 
     spawn: function(role, specification) {
-        let creepName, spawnName;
-
+        var creepName = this.getName(role);
+        var reValue;
+        
         switch(role) {
             case 'simple_harvester':
-                Game.spawns[spawnName].spawnCreep([WORK, CARRY, CARRY, MOVE], creepName, {memory: {role: 'simple_harvester', target: specification}});
-                console.log('manager.creep_spawner>>: ' + role + 'with name: ' + creepName + 'at' + spawnName + 'created');
+                reValue = Game.spawns[this.spawnName].spawnCreep([WORK, CARRY, CARRY, MOVE], creepName, {memory: {role: 'simple_harvester', target: specification}});
                 break;
             case 'sationary_harvester':
-                Game.spawns[spawnName].spawnCreep([], creepName, {memory: {role: 'sationary_harvester', target: specification}});
-                console.log('manager.creep_spawner>>: ' + role + 'with name: ' + creepName + 'at' + spawnName + 'created');
+                reValue = Game.spawns[this.spawnName].spawnCreep([WORK, WORK, MOVE], creepName, {memory: {role: 'sationary_harvester', target: specification}});
                 break;
             case 'carrier':
-                Game.spawns[spawnName].spawnCreep([], creepName, {memory: {role: 'carrier'}});
-                console.log('manager.creep_spawner>>: ' + role + 'with name: ' + creepName + 'at' + spawnName + 'created');
+                reValue = Game.spawns[this.spawnName].spawnCreep([CARRY, CARRY, MOVE, MOVE], creepName, {memory: {role: 'carrier'}});
                 break;
             case 'upgrader':
-                Game.spawns[spawnName].spawnCreep([WORK, CARRY, CARRY, MOVE], creepName, {memory: {role: 'upgrader'}});
-                console.log('manager.creep_spawner>>: ' + role + 'with name: ' + creepName + 'at' + spawnName + 'created');
+                reValue = Game.spawns[this.spawnName].spawnCreep([WORK, CARRY, MOVE, MOVE], creepName, {memory: {role: 'upgrader'}});
                 break;
             case 'builder':
-                Game.spawns[spawnName].spawnCreep([WORK, CARRY, CARRY, MOVE], creepName, {memory: {role: 'builder'}});
-                console.log('manager.creep_spawner>>: ' + role + 'with name: ' + creepName + 'at' + spawnName + 'created');
+                reValue = Game.spawns[this.spawnName].spawnCreep([WORK, CARRY, CARRY, MOVE], creepName, {memory: {role: 'builder'}});
                 break;
             case 'militia':
-                Game.spawns[spawnName].spawnCreep([ATTACK, ATTACK, MOVE, MOVE], creepName, {memory: {role: 'militia', designatedRoom: specification}});
-                console.log('manager.creep_spawner>>: ' + role + 'with name: ' + creepName + 'at' + spawnName + 'created');
+                reValue = Game.spawns[this.spawnName].spawnCreep([ATTACK, ATTACK, MOVE, MOVE], creepName, {memory: {role: 'militia', designatedRoom: specification}});
                 break;
             default:
+                reValue = 1;
                 break;
           }
 
+        switch(reValue) {
+            case 0:
+                console.log('manager.creep_spawner>>: ' + role + 'with name: ' + creepName + 'at' + spawnName + 'created');
+                break;
+            case -1:
+                console.log('manager.creep_spawner>>: ERR_NOT_OWNER');
+                break;
+            case -3:
+                console.log('manager.creep_spawner>>: ERR_NAME_EXISTS');
+                this.commitName(role);
+                this.spawn(role, specification);
+                break;
+            case -4:
+                console.log('manager.creep_spawner>>: ERR_BUSY');
+                break;
+            case -6:
+                console.log('manager.creep_spawner>>: ERR_NOT_ENOUGH_ENERGY');
+                break;
+            case -10:
+                console.log('manager.creep_spawner>>: ERR_INVALID_ARGS');
+                break;
+            case -14:
+                console.log('manager.creep_spawner>>: ERR_RCL_NOT_ENOUGH');
+                break;
+            default:
+                console.log('manager.creep_spawner>>: role doesn_t exist');
+                break;
+        }
     },
 
-    find_missing_creeps: function() {
+    missing_stationary_harvester: function() {
+        var data = Game.spawns[this.spawnName].memory.sources;
+
+        for(var i in data) {
+            // Game.getObjectById(data[i]);
+            // Game.spawns[this.spawnName].room.find(FIND_MY_CREEPS, {})
+            
+            /* Creating Array with all stationary Harvester Creeps for a Source to check if there are enough */
+            var harvesters = [];
+            for(var i in Game.creeps) {
+                if(Game.creeps[i].memory.role == 'sationary_harvester' && Game.creeps[i].memory.target == i) {
+                    harvesters.push(Game.creeps[i]);
+                }
+            }
+            if(harvesters.length < data[i] && harvesters.length < this.maxSHpS) {
+                this.spawn('sationary_harvester', i);
+                return true;
+            }
+        }
+        return false;
+    },
+
+    missing_carriers: function() {
+        var data = Game.spawns[this.spawnName].memory.sources;
+        var creepName = 'Carrier';
+        var counter = 0;
+
+        for(var i in data) {
+            if(data[i] > this.maxSHpS) {
+                counter += this.maxSHpS;
+            }
+            else {
+                counter += data[i];
+            }
+        }
+
+        var carriers = [];
+        for(var i in Game.creeps) {
+            if(Game.creeps[i].memory.role == 'carrier') {
+                carriers.push(Game.creeps[i]);
+            }
+        }
+        if(carriers.length < counter) {
+            this.spawn('carrier', undefined);
+            return true;
+        }
+
+        return false;
+    },
+
+    missing_upgrader: function() {
+        if (Memory.rooms.spawnName.maxCreeps === undefined)
+            Memory.rooms.spawnName.maxCreeps = {};
+        
+        if (Memory.rooms.spawnName.maxCreeps['upgrader'] === undefined) 
+            Memory.rooms.spawnName.maxCreeps['upgrader'] = 0;
+
+        var upgraders = [];
+        for(var i in Game.creeps) {
+            if(Game.creeps[i].memory.role == 'upgrader') {
+                upgraders.push(Game.creeps[i]);
+            }
+        }
+        if(upgraders.length < Memory.rooms.spawnName.maxCreeps['upgrader']) {
+            this.spawn('upgrader', undefined);
+            return true;
+        }
+
+        return false;
+
+    },
+
+    missing_simple_harvester: function() {
+        if (Memory.rooms.spawnName.maxCreeps === undefined)
+            Memory.rooms.spawnName.maxCreeps = {};
+        
+        if (Memory.rooms.spawnName.maxCreeps['simple_harvester'] === undefined) 
+            Memory.rooms.spawnName.maxCreeps['simple_harvester'] = 0;
+
+        var simple_harvesters = [];
+        for(var i in Game.creeps) {
+            if(Game.creeps[i].memory.role == 'simple_harvester') {
+                simple_harvesters.push(Game.creeps[i]);
+            }
+        }
+        if(simple_harvesters.length < Memory.rooms.spawnName.maxCreeps['simple_harvester']) {
+            this.spawn('simple_harvester', undefined);
+            return true;
+        }
+
+        return false;
+
+    },
+
+    missing_builder: function() {
+        if (Memory.rooms.spawnName.maxCreeps === undefined)
+            Memory.rooms.spawnName.maxCreeps = {};
+        
+        if (Memory.rooms.spawnName.maxCreeps['builder'] === undefined) 
+            Memory.rooms.spawnName.maxCreeps['builder'] = 0;
+
+        var upgraders = [];
+        for(var i in Game.creeps) {
+            if(Game.creeps[i].memory.role == 'builder') {
+                upgraders.push(Game.creeps[i]);
+            }
+        }
+        if(upgraders.length < Memory.rooms.spawnName.maxCreeps['builder']) {
+            this.spawn('builder', undefined);
+            return true;
+        }
+
+        return false;
+
+    },
+
+    missing_militia: function() {
+        if (Memory.rooms.spawnName.maxCreeps === undefined)
+            Memory.rooms.spawnName.maxCreeps = {};
+        
+        if (Memory.rooms.spawnName.maxCreeps['militia'] === undefined) 
+            Memory.rooms.spawnName.maxCreeps['militia'] = 0;
+
+        var militias = [];
+        for(var i in Game.creeps) {
+            if(Game.creeps[i].memory.role == 'militia') {
+                militias.push(Game.creeps[i]);
+            }
+        }
+        if(militias.length < Memory.rooms.spawnName.maxCreeps['militia']) {
+            this.spawn('militia', undefined);
+            return true;
+        }
+
+        return false;
 
     }
 
